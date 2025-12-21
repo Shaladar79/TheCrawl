ed4dee513f7208e9bba03e496eb65c5807beeebb
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-21 12:56:08 -0600

    Update system.mjs

--- system.mjs
+++ system.mjs
@@ -1,1 +1,9 @@
+// system.mjs (root)
+// Keep this file small. Everything should be imported from /scripts.
+
+import { initTheCrawl } from "./scripts/init.mjs";
+
+Hooks.once("init", async () => {
+  await initTheCrawl();
+});
 
