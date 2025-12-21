6493a144a1572a5ec26ec526b012b1a7ac230924
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-21 13:05:14 -0600

    Update preload-templates.mjs

--- scripts/templates/preload-templates.mjs
+++ scripts/templates/preload-templates.mjs
@@ -1,11 +1,8 @@
 // scripts/templates/preload-templates.mjs
 export async function preloadHandlebarsTemplates() {
-  // Add template paths here when you create .hbs files.
   const templatePaths = [
-    // "systems/thecrawl/templates/actor/actor-sheet.hbs"
+    "systems/thecrawl/templates/actor/actor-sheet.hbs"
   ];
 
-  if (templatePaths.length) {
-    await loadTemplates(templatePaths);
-  }
+  await loadTemplates(templatePaths);
 }
