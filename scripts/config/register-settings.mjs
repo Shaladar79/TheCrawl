003b019012804f1abddfb6445c804dba17df4f11
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-21 13:01:23 -0600

    Create register-settings.mjs

---
+++ scripts/config/register-settings.mjs
@@ -0,0 +1,6 @@
+// scripts/config/register-settings.mjs
+export function registerSystemSettings() {
+  // Register settings later if needed.
+  // Example:
+  // game.settings.register("thecrawl", "debug", { name: "Debug", scope: "client", config: true, type: Boolean, default: false });
+}
