a2988bee023015a7b511c00ade6f9b5766bd7a9c
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-21 13:32:16 -0600

    Update register-sheets.mjs

--- scripts/sheets/register-sheets.mjs
+++ scripts/sheets/register-sheets.mjs
@@ -1,13 +1,21 @@
 // scripts/sheets/register-sheets.mjs
 import { TheCrawlActorSheet } from "./actor-sheet.mjs";
+import { TheCrawlTalentSheet } from "./skill-sheet.mjs";
 
 export function registerSheets() {
+  // Actor sheet
   Actors.unregisterSheet("core", ActorSheet);
-
   Actors.registerSheet("thecrawl", TheCrawlActorSheet, {
     types: ["pc", "npc", "monster", "companion", "summon"],
     makeDefault: true
   });
 
-  console.log("The Crawl | Actor sheet registered");
+  // Item sheet: Talent (Item type "skill")
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("thecrawl", TheCrawlTalentSheet, {
+    types: ["skill"],
+    makeDefault: true
+  });
+
+  console.log("The Crawl | Sheets registered");
 }
