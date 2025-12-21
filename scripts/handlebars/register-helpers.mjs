56185e2b81bd4fbf686b211869d6d4b574205f6d
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-21 14:00:18 -0600

    Update register-helpers.mjs

--- scripts/handlebars/register-helpers.mjs
+++ scripts/handlebars/register-helpers.mjs
@@ -1,9 +1,13 @@
 // scripts/handlebars/register-helpers.mjs
 export function registerHandlebarsHelpers() {
   Handlebars.registerHelper("eq", (a, b) => a === b);
+
   Handlebars.registerHelper("or", function () {
-    // Usage: (or cond1 cond2 cond3 ...)
     const args = Array.from(arguments).slice(0, -1);
     return args.some(Boolean);
   });
+
+  Handlebars.registerHelper("json", (context) =>
+    JSON.stringify(context ?? {}, null, 2)
+  );
 }
