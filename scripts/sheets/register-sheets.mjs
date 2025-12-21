81d23bfa6e61c63eae2c496d7b77692bba420fdc
Shaladar79 <scott_d_anderson79@yahoo.com>
2025-12-21 16:16:12 -0600

    Update register-sheets.mjs

--- scripts/sheets/register-sheets.mjs
+++ scripts/sheets/register-sheets.mjs
@@ -9,50 +9,20 @@ import { TheCrawlRaceSheet } from "./race-sheet.mjs";
 import { TheCrawlEquipmentSheet } from "./equipment-sheet.mjs";
 
 export function registerSheets() {
-
-  /* -------------------------------------------- */
-  /* Actor Sheet                                  */
-  /* -------------------------------------------- */
-
+  // Actor sheet
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("thecrawl", TheCrawlActorSheet, {
     types: ["pc", "npc", "monster", "companion", "summon"],
     makeDefault: true
   });
 
-  /* -------------------------------------------- */
-  /* Item Sheets                                  */
-  /* -------------------------------------------- */
-
-  // Explicitly unregister core sheets by type
+  // Item sheets
   Items.unregisterSheet("core", ItemSheet);
 
-  // Skill
-  Items.registerSheet("thecrawl", TheCrawlSkillSheet, {
-    types: ["skill"],
-    makeDefault: true
-  });
-
-  // Talent
-  Items.registerSheet("thecrawl", TheCrawlTalentSheet, {
-    types: ["talent"],
-    makeDefault: true
-  });
-
-  // Race
-  Items.registerSheet("thecrawl", TheCrawlRaceSheet, {
-    types: ["race"],
-    makeDefault: true
-  });
-
-  // Equipment
-  Items.registerSheet("thecrawl", TheCrawlEquipmentSheet, {
-    types: ["equipment"],
-    makeDefault: true
-  });
-
-  console.log("The Crawl | Sheets registered");
-}
+  Items.registerSheet("thecrawl", TheCrawlSkillSheet, { types: ["skill"], makeDefault: true });
+  Items.registerSheet("thecrawl", TheCrawlTalentSheet, { types: ["talent"], makeDefault: true });
+  Items.registerSheet("thecrawl", TheCrawlRaceSheet, { types: ["race"], makeDefault: true });
+  Items.registerSheet("thecrawl", TheCrawlEquipmentSheet, { types: ["equipment"], makeDefault: true });
 
   console.log("The Crawl | Sheets registered");
 }
